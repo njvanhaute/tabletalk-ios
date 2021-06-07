1b7633bb70ee0c162422e239a7bdc7a6ab90be7c
Nicholas Vanhaute <njvanhaute@gmail.com>
2021-06-07 18:50:03 -0500

    Commit initial files

---
+++ tabletalk-ios/tabletalk_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  tabletalk_iosApp.swift
+//  tabletalk-ios
+//
+//  Created by Nicholas Vanhaute on 6/7/21.
+//
+
+import SwiftUI
+
+@main
+struct tabletalk_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
